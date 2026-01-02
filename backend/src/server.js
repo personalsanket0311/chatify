8c356656c7e367c98adc9cff513d50b9b19e6d1c
sanket kanade <sanketkanade2525@gmail.com>
2026-01-02 08:00:28 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import authRoute from './routes/auth.route.js';
 import messageRoute from './routes/message.routes.js';
@@ -7,10 +8,25 @@ import messageRoute from './routes/message.routes.js';
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT  || 3000
+// Add these middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
 
-app.listen(PORT, () =>  console.log('Server is running on port: ' + PORT));
+// Fix the NODE_ENV check and paths
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/chatify/dist")));
+    
+    app.get("*", (_, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/chatify/dist/index.html"));
+    });
+}
+
+console.log("NODE_ENV:", process.env.NODE_ENV);
+
+app.listen(PORT, () => console.log('Server is running on port: ' + PORT));
